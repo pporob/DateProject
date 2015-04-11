98a58d25385f0ad88fadefbe51d0d069796a817b
pporob <pporob00@fesb.hr>
2015-04-11 20:37:04 +0200

    Tests

---
+++ DateProject/Date.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateProject
+{
+    public class Date
+    {
+        public int day, month, year;
+
+
+        public Date(int day, int month, int year)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+
+
+
+        public bool isLeapYear()
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                return true;
+            else return false;
+        }
+
+        public string getMonthName()
+        {
+            string[] MonthName = { "Sijecanj", "Veljaca", "Ozujak", "Travanj", "Svibanj", "Lipanj", "Srpanj", "Kolovoz", "Rujan", "Listopad", "Studeni", "Prosinac" };
+            return MonthName[month - 1];
+        }
+
+        public int getNumberOfRemaingDaysInMonth()
+        {
+            if ((month % 2 == 1 && month <= 7) || (month % 2 == 0 && month >= 8))
+            {
+                return 31 - day;
+            }
+            else if (month == 2)
+            {
+                if (isLeapYear())
+                    return 29 - day;
+                else
+                    return 28 - day;
+            }
+            else
+                return 30 - day;
+        }
+    }
+
+}
+
+
+
+
+
---
+++ DateTestProject/DateTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DateProject;
+
+namespace DateTestProject
+{
+    [TestClass]
+    public class DateTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Date date = new Date(10,10,2010);
+
+            Assert.AreEqual(date.day, 10, "Day not correct.");
+            Assert.AreEqual(date.month, 10, "Month not correct.");
+            Assert.AreEqual(date.year, 2010, "Year not correct.");
+        }
+
+        [TestMethod]
+        public void MonthNameTest()
+        {
+            var date = new Date(10, 10, 2010);
+            var month = date.getMonthName();
+            Assert.AreEqual(month, "Listopad", "Month name not correct.");
+        }
+
+        [TestMethod]
+        public void NumberOfRemaingDaysInMonthTest()
+        {
+            var date = new Date(10, 10, 2010);
+            var remDays = date.getNumberOfRemaingDaysInMonth();
+            Assert.AreEqual(remDays, 21, "Number of remaining days in month is not correct.");
+        }
+
+        [TestMethod]
+        public void LeapYearTest()
+        {
+            var date = new Date(10, 10, 2012);
+            Assert.AreEqual(date.isLeapYear(), true, "Leap year not detected.");
+        }
+
+        [TestMethod]
+        public void NotLeapYearTest()
+        {
+            var date = new Date(10, 10, 2010);
+            Assert.AreEqual(date.isLeapYear(), false, "Leap year falsely detected.");
+        }
+
+    }
+}
